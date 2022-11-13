eef2294defc24e6482087be6a02d69558af24dbd
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-11-13 20:50:05 +0100

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthContextProvider } from '../src/context/AuthContext';
 import { useRouter } from 'next/router'
 import ProtectedRoute from '../src/components/ProtectedRoute'
-import 'bootswatch/dist/cosmo/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+import React, { useEffect } from 'react';
 
 const noAuthRequired = ['/',
                        '/login',
@@ -12,6 +13,11 @@ const noAuthRequired = ['/',
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+  
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <AuthContextProvider>
       {noAuthRequired.includes(router.pathname) ? (
